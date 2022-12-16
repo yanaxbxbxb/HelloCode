892b45cfd2623e187a54adf78c864214dbb86b5e
shakirova_yana <ayna.shakirova68@gmail.com>
2022-12-16 21:19:23 +0300

    change example 007

--- example007_Magic/Program.cs
+++ example007_Magic/Program.cs
@@ -1,6 +1,6 @@
 ﻿Console.Clear();
-//Console.SetCursorPosition(10, 4);
-//Console.WriteLine("+");
+Console.SetCursorPosition(10, 4);
+Console.WriteLine("+");
 
 int xa = 40, ya = 1,
     xb = 1, yb = 30,
@@ -19,27 +19,33 @@ Console.WriteLine("+");
 int x = xa, y = xb;
 int count = 0;
 while (count < 10000)
-{
-    int what = new Random().Next(0, 3); // [0;3)
-    if (what == 0)
-    {
-        x = (x+xa)/2;
-        y = (y+ya)/2;
-    }
+   {
+     int what = new Random().Next(0, 3); // [0;3)
+     if (what == 0)
+   {
+         x = (x+xa)/2;
+         y = (y+ya)/2;
+   }
 
-      if (what == 1)
-    {
-        x = (x+xb)/2;
-        y = (y+yb)/2;
-    }
+       if (what == 1)
+     {
+         x = (x+xb)/2;
+         y = (y+yb)/2;
+     }
 
-      if (what == 2)
-    {
-        x = (x+xc)/2;
-        y = (y+yc)/2;
-    }
+       if (what == 2)
+     {
+         x = (x+xc)/2;
+         y = (y+yc)/2;
+     }
 
-    Console.SetCursorPosition(x, y);
-    Console.WriteLine("+");
-    count +=1;
-}
+     Console.SetCursorPosition(x, y);
+     Console.WriteLine("+");
+     count +=1;
+ }
+int numberA = new Random().Next(1, 10); // 1-9
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 10);
+ Console.WriteLine(numberB);
+ int result = numberA + numberB;
+ Console.WriteLine(result);
