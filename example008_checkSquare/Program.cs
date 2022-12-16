0aecbdb5dacea6ecbfab062602a8b49a34568dd2
shakirova_yana <ayna.shakirova68@gmail.com>
2022-12-16 23:12:49 +0300

    add example008

---
+++ example008_checkSquare/Program.cs
@@ -0,0 +1,17 @@
+﻿int i = int.Parse(Console.ReadLine());
+
+if (int.TryParse(Console.ReadLine(), out int j))
+{
+    if (i == Math.Pow(j, 2))
+    {
+        Console.WriteLine($"Число {i} является квадратом числа {j}");
+    }
+    else
+    {
+        Console.WriteLine($"Число {i} не является квадратом числа {j}");
+    }
+}
+else
+{
+    Console.WriteLine($"Это не число!");
+}
