690e3cfc8ec29154b992c0ae9ce88fcc27c8e666
shakirova_yana <ayna.shakirova68@gmail.com>
2022-12-16 23:12:04 +0300

    add example 010

--- example010_N/Program.cs
+++ example010_N/Program.cs
@@ -1,26 +1,8 @@
-﻿// int n = int.Parse(Console.ReadLine());
-// int count = (-n);
+﻿ int n = int.Parse(Console.ReadLine());
+ int count = (-n);
 
-// while (count <= n)
-// {
-//     Console.Write($"{count}, ");
-//     count = count +1;
-// }
-
-Console.Write("Введите трехзначное число: ");
-if (int.TryParse(Console.ReadLine(), out int n))
-{
-        if (n >= 100 && n < 1000)
-        {
-            int x = n % 10;
-            Console.Write($"Последняя цифра вашего числа {x}");
-        }
-        else 
-        {
-            System.Console.WriteLine("Вы ввели не трехзначное число!");
-        }
-}
-else 
-{
-    System.Console.WriteLine("Вы ввели не число!");
-}
+ while (count <= n)
+ {
+     Console.Write($"{count}, ");
+     count = count +1;
+ }
