55c07db43ce2812ae13b57f836b1df0a9cc42b1f
shakirova_yana <ayna.shakirova68@gmail.com>
2022-12-16 21:15:25 +0300

    add example 009 Day Of Week

---
+++ example009_DayOfWeek/Program.cs
@@ -0,0 +1,36 @@
+﻿int a = int.Parse(Console.ReadLine());
+
+if (a == 1)
+   {
+    Console.WriteLine("Сегодня Понедельник");
+   } 
+
+else if (a == 2)
+   {
+    Console.WriteLine("Сегодня Вторник");
+   } 
+
+else if (a == 3)
+   {
+    Console.WriteLine("Сегодня Среда");
+   } 
+else if (a == 4)
+   {
+    Console.WriteLine("Сегодня Четверг");
+   } 
+else if (a == 5)
+   {
+    Console.WriteLine("Сегодня Пятница");
+   } 
+else if (a == 6)
+   {
+    Console.WriteLine("Сегодня Суббота");
+   } 
+else if (a == 7)
+   {
+    Console.WriteLine("Сегодня Воскресенье");
+   } 
+else 
+   {
+   Console.WriteLine("Дня недели с таким номером нет!");
+   }
