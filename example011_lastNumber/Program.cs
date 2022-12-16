0fb5fb64782327cdaac250ae246a47157a4e226d
shakirova_yana <ayna.shakirova68@gmail.com>
2022-12-16 23:09:54 +0300

    add example 011

---
+++ example011_lastNumber/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введите трехзначное число: ");
+if (int.TryParse(Console.ReadLine(), out int n))
+{
+        if (n >= 100 && n < 1000)
+        {
+            int x = n % 10;
+            Console.Write($"Последняя цифра вашего числа {x}");
+        }
+        else 
+        {
+            System.Console.WriteLine("Вы ввели не трехзначное число!");
+        }
+}
+else 
+{
+    System.Console.WriteLine("Вы ввели не число!");
+}
