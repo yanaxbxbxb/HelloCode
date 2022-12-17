7e982a769295f78f6696ec00fa84f5bc6cf816e8
shakirova_yana <ayna.shakirova68@gmail.com>
2022-12-17 17:13:39 +0300

    add example 014 check number for parity

---
+++ example014_checkForParity/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число: ");
+int a = int.Parse(Console.ReadLine());
+
+if (a % 2 == 0)
+{
+     Console.Write($"Число {a} - четное");
+}
+else
+{
+     Console.Write($"Число {a} - нечетное");
+}
