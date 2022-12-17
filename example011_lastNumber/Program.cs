db30253c6c265e93fc562e698010e6cce19bf96d
shakirova_yana <ayna.shakirova68@gmail.com>
2022-12-17 17:43:41 +0300

    example 011 change spaces

--- example011_lastNumber/Program.cs
+++ example011_lastNumber/Program.cs
@@ -1,15 +1,15 @@
 ﻿Console.Write("Введите трехзначное число: ");
 if (int.TryParse(Console.ReadLine(), out int n))
 {
-        if (n >= 100 && n < 1000)
-        {
-            int x = n % 10;
-            Console.Write($"Последняя цифра вашего числа {x}");
-        }
-        else 
-        {
-            System.Console.WriteLine("Вы ввели не трехзначное число!");
-        }
+    if (n >= 100 && n < 1000)
+    {
+        int x = n % 10;
+        Console.Write($"Последняя цифра вашего числа {x}");
+    }
+    else 
+    {
+        System.Console.WriteLine("Вы ввели не трехзначное число!");
+    }
 }
 else 
 {
