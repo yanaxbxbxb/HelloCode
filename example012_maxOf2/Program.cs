31c698fe9c89f5ce17e725a14f567b43cb95c5f7
shakirova_yana <ayna.shakirova68@gmail.com>
2022-12-17 16:30:15 +0300

    add example 012 max of 2 numbers

---
+++ example012_maxOf2/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите первое число: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b = int.Parse(Console.ReadLine());
+int max = 0;
+
+if (a > b) max = a;
+else if (b > a) max = b;
+
+Console.Write("max = ");
+Console.WriteLine(max);
