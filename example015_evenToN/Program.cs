03fbcd140cb83aae75149d3a5999f11cbeadcbf2
shakirova_yana <ayna.shakirova68@gmail.com>
2022-12-17 17:26:24 +0300

    add example 015 print even to n

---
+++ example015_evenToN/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число: ");
+int a = int.Parse(Console.ReadLine());
+int count = 1;
+while (count <= a)
+{
+    if (count % 2 == 0)
+    {
+        Console.Write($"{count} ");
+    }
+     count = count + 1;
+}
