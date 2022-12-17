3e500fde4c7d3912345686c357404229438be139
shakirova_yana <ayna.shakirova68@gmail.com>
2022-12-17 16:48:02 +0300

    add example 013 max of 3 numbers

---
+++ example013_maxOf3/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.Write("Введите первое число: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b = int.Parse(Console.ReadLine());
+Console.Write("Введите третье число: ");
+int c = int.Parse(Console.ReadLine());
+int max = 0;
+
+if (a > b)
+{
+    if (a > c) max = a; 
+    else if (c > a) max = c;
+}
+
+else if (b > a) 
+{
+    if (b > c) max = b;
+    else if (c > b) max = c;
+}
+
+else if (c > a)
+{
+    if (c > b) max = c;
+    else if (b > c) max = b;
+}
+
+Console.Write("max = ");
+Console.WriteLine(max);
