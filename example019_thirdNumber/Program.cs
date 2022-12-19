85cdd512aa42f6fc3ea407e0b525c835de43dece
shakirova_yana <ayna.shakirova68@gmail.com>
2022-12-19 18:06:43 +0300

    add example 019 find third number

---
+++ example019_thirdNumber/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+int[] arr = number.ToString().ToCharArray().Select(x => x - '0').ToArray();
+if (number >= 100)
+{
+    int thirdNumber = arr[2];
+    Console.WriteLine($"Третья цифра числа - {thirdNumber}");
+}
+else 
+{
+    Console.WriteLine("В числе меньше трех цифр!");
+}
