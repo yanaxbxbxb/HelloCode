9a4372c6c74b727fd6efcfd1c105c977472b1e86
shakirova_yana <ayna.shakirova68@gmail.com>
2022-12-19 18:05:27 +0300

    add example 018 print second number

---
+++ example018_secondNumber/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число 
+//  и на выходе показывает вторую цифру этого числа.
+
+Console.Write("Введите число от 100 до 999: ");
+int number = int.Parse(Console.ReadLine());
+if (number >= 100 && number <= 999)
+{
+    int secondNumber = (number / 10) % 10;
+    Console.WriteLine($"Вторая цифра числа - {secondNumber}");
+}
+else 
+{
+    Console.WriteLine("Вы ввели не трехзначное число!");
+}
