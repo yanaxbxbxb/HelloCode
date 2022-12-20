af54c55862c5cf2cae3168e7d9e5d2b8f59fb3d7
shakirova_yana <ayna.shakirova68@gmail.com>
2022-12-20 16:58:29 +0300

    add example 025

---
+++ example025_distance3D/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+Console.Write("Введите координату x для точки A: ");
+int xA = int.Parse(Console.ReadLine());
+Console.Write("Введите координату y для точки A: ");
+int yA = int.Parse(Console.ReadLine());
+Console.Write("Введите координату z для точки A: ");
+int zA = int.Parse(Console.ReadLine());
+Console.Write("Введите координату x для точки B: ");
+int xB = int.Parse(Console.ReadLine());
+Console.Write("Введите координату y для точки B: ");
+int yB = int.Parse(Console.ReadLine());
+Console.Write("Введите координату z для точки B: ");
+int zB = int.Parse(Console.ReadLine());
+double distance = 0;
+
+// √((xb - xa)2 + (yb - ya)2 + (zb - za)2)
+
+distance = Math.Sqrt(Math.Pow(xA - xB, 2) + Math.Pow(yA - yB, 2) + Math.Pow(zA - zB, 2));
+Console.WriteLine(Math.Round(distance, 2));
+
+
+
+
+
