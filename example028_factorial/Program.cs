8a632346dbddccb5dd99e15b91879c7a9cbf2792
shakirova_yana <ayna.shakirova68@gmail.com>
2022-12-20 22:26:25 +0300

    add example 028 factorial

---
+++ example028_factorial/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+void TaskFactorial()
+{
+    Console.WriteLine("TaskFactorial");
+    int N = ReadInt("N");
+    int factorial = Factorial(N);
+    Console.WriteLine(factorial);
+}
+
+int ReadInt(string argumentName)
+{
+	Console.Write($"Input {argumentName}: ");
+	return int.Parse(Console.ReadLine());
+}
+
+int Factorial(int N)
+{
+    int result = 1;
+    for (int i = 1; i <= N; i++)
+        {
+            result = result * i;
+        }
+    return result;
+}
+
+TaskFactorial();
