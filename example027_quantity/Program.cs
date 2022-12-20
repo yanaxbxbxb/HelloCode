5a80c8623c1b92130d301632222e31957e8eb6a0
shakirova_yana <ayna.shakirova68@gmail.com>
2022-12-20 22:25:49 +0300

    add example027 количество цифр в числе

---
+++ example027_quantity/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе
+
+void TaskQuantity()
+{
+	Console.WriteLine("TaskQuantity");
+	string number = ReadInt("number").ToString();
+    
+	Console.WriteLine($"Количество цифр в числе - {number.Length}");
+}
+
+int ReadInt(string argumentName)
+{
+	Console.Write($"Input {argumentName}: ");
+	return int.Parse(Console.ReadLine());
+}
+
+TaskQuantity();
