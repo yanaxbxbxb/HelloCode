0d5ac6b91089919cbe70a14474a15f13a8220e02
shakirova_yana <ayna.shakirova68@gmail.com>
2022-12-22 18:28:56 +0300

    add example031

---
+++ example031_sumInNumber/Program.cs
@@ -0,0 +1,29 @@
+﻿//  Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+void SumInNumber()
+{
+    string number = ReadInt("number").ToString();
+
+    int result = Sum_of_digits(number);
+
+    Console.Write($"Sum of digits in number {number} is {result}");
+}
+
+SumInNumber();
+
+int ReadInt(string argumentName)
+{
+	Console.Write($"Input {argumentName}: ");
+	return int.Parse(Console.ReadLine()!);
+}
+
+int Sum_of_digits(string number)
+{
+    int result = 0;
+    for (int i = 0; i < number.Length; i++)
+    {
+        int a = int.Parse(number[i].ToString());
+        result = result + a;
+    }
+    return result;
+}
