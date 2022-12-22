77b6d85a78d467b2e31f26eb693e49edb05f8675
shakirova_yana <ayna.shakirova68@gmail.com>
2022-12-22 18:28:24 +0300

    add example 030

---
+++ example030_A_degree_B/Program.cs
@@ -0,0 +1,29 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+void A_degree_B()
+{
+    int A = ReadInt("A");
+    int B = ReadInt("B");
+
+    int result = A_deg_B(A, B);
+    Console.Write($"Number {A} in the power of {B} is {result}");
+}
+
+A_degree_B();
+
+int ReadInt(string argumentName)
+{
+	Console.Write($"Input {argumentName}: ");
+	return int.Parse(Console.ReadLine()!);
+}
+
+int A_deg_B(int A, int B)
+{
+    int result = 1;
+    for (int i = 0; i < B; i++)
+    {
+        result = result * A;
+    }
+    return result;
+}
+
