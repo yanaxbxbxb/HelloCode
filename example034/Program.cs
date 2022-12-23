5b27699014dbdbdece648d36c21a5c4cbfeeebab
shakirova_yana <ayna.shakirova68@gmail.com>
2022-12-23 18:20:27 +0300

    example034

---
+++ example034/Program.cs
@@ -0,0 +1,50 @@
+﻿// Задайте массив. Напишите программу, которая определяет, присутствует ли заданное число в массиве.
+
+int length = ReadInt("Array length");
+int[] array = GetRandomArray(length, -100, 100);
+
+PrintArray(array);
+Search(array, ReadInt("Число, которое пользователь хочет найти"));
+System.Console.WriteLine();
+
+
+int ReadInt(string argument)
+{
+	Console.Write($"Input {argument}: ");
+	return int.Parse(Console.ReadLine()!);
+}
+
+int[] GetRandomArray(int length, int minValue, int maxValue)
+{
+	int[] array = new int[length];
+	Random random = new Random();
+
+	for (int i = 0; i < array.Length; i++)
+	{
+		array[i] = random.Next(minValue, maxValue + 1);
+	}
+
+	return array;
+}
+
+
+void PrintArray(int[] array)
+{
+	for (int i = 0; i < array.Length; i++)
+	{
+		Console.Write($"{array[i]}, ");
+	}
+}
+
+void Search(int[] array, int x)    
+{
+    for (int i = 0; i < array.Length; i++)
+	{
+		if (x==array[i])
+        {
+            Console.WriteLine("YES");
+            return;
+        }
+	}
+    System.Console.WriteLine("NO");
+}
