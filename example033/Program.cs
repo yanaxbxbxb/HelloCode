95248ff62259f6f568a8e289ca512e756727c0df
shakirova_yana <ayna.shakirova68@gmail.com>
2022-12-23 18:20:08 +0300

    add example033

---
+++ example033/Program.cs
@@ -0,0 +1,45 @@
+﻿// Напишите программу замена элементов массива: положительные элементы замените на соответствующие отрицательные, и наоборот.
+
+int length = ReadInt("Array length");
+int[] array = GetRandomArray(length, -9, 9);
+
+PrintArray(array);
+plusminus(array);
+System.Console.WriteLine();
+PrintArray(array);
+
+int ReadInt(string argument)
+{
+	Console.Write($"Input {argument}: ");
+	return int.Parse(Console.ReadLine()!);
+}
+
+int[] GetRandomArray(int length, int minValue, int maxValue)
+{
+	int[] array = new int[length];
+	Random random = new Random();
+
+	for (int i = 0; i < array.Length; i++)
+	{
+		array[i] = random.Next(minValue, maxValue + 1);
+	}
+
+	return array;
+}
+
+
+void PrintArray(int[] array)
+{
+	for (int i = 0; i < array.Length; i++)
+	{
+		Console.Write($"{array[i]}, ");
+	}
+}
+
+void plusminus(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+	{
+		array[i] = array[i] * -1;
+	}
+}
