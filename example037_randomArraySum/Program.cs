fa2af758c4a7e920e6f40925a7fb525cd0f862a9
shakirova_yana <ayna.shakirova68@gmail.com>
2022-12-23 18:15:28 +0300

    add example 038

---
+++ example037_randomArraySum/Program.cs
@@ -0,0 +1,45 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+
+int length = ReadInt("Array length");
+int[] array = GetRandomArray(length, 0, 100);
+
+PrintArray(array);
+System.Console.WriteLine();
+SumOfElementsOddPosition(array);
+
+int ReadInt(string argument)
+{
+	Console.Write($"Input {argument}: ");
+	return int.Parse(Console.ReadLine());
+}
+
+int[] GetRandomArray(int length, int minValue, int maxValue)
+{
+	int[] array = new int[length];
+	Random random = new Random();
+
+	for (int i = 0; i < array.Length; i++)
+	{
+		array[i] = random.Next(minValue, maxValue + 1);
+	}
+
+	return array;
+}
+
+void PrintArray(int[] array)
+{
+	for (int i = 0; i < array.Length; i++)
+	{
+		Console.Write($"{array[i]}, ");
+	}
+}
+
+void SumOfElementsOddPosition(int[] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i+=2)
+    {
+        sum = sum + array[i];
+    }
+    System.Console.WriteLine($"The sum of elements on odd positions is {sum}");
+}
