cf4ab20b21e9d1bb8cefacd7a901fa01f0d39a77
shakirova_yana <ayna.shakirova68@gmail.com>
2022-12-23 18:14:52 +0300

    add example036

---
+++ example036_randomArrayCountEven/Program.cs
@@ -0,0 +1,46 @@
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+
+int length = ReadInt("Array length");
+int[] array = GetRandomArray(length, 100, 999);
+
+PrintArray(array);
+System.Console.WriteLine();
+CountEven(array);
+
+int ReadInt(string argument)
+{
+	Console.Write($"Input {argument}: ");
+	return int.Parse(Console.ReadLine());
+}
+
+int[] GetRandomArray(int length, int minValue, int maxValue)
+{
+	int[] array = new int[length];
+	Random random = new Random();
+
+	for (int i = 0; i < array.Length; i++)
+	{
+		array[i] = random.Next(minValue, maxValue + 1);
+	}
+
+	return array;
+}
+
+void PrintArray(int[] array)
+{
+	for (int i = 0; i < array.Length; i++)
+	{
+		Console.Write($"{array[i]}, ");
+	}
+}
+
+void CountEven(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0) {count++;}
+    }
+    System.Console.WriteLine($"The amount of even numbers in array is {count}");
+}
