01e2c562a300d8648bc3d12817372ccd723e61b8
shakirova_yana <ayna.shakirova68@gmail.com>
2022-12-25 16:08:27 +0300

    add example 041 intersection

---
+++ example041_intersection/Program.cs
@@ -0,0 +1,38 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями 
+// y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+double k1 = ReadInt("k1");
+double k2 = ReadInt("k2");
+double b1 = ReadInt("b1");
+double b2 = ReadInt("b2");
+
+Intersection(k1, k2, b1, b2);
+
+double ReadInt(string argument)
+{
+	Console.Write($"Input {argument}: ");
+	return double.Parse(Console.ReadLine()!);
+}
+
+void Intersection(double k1, double k2, double b1, double b2)
+{
+    if (k1 != k2 && b1 != b2)
+    {
+        double x = 0;
+        double y = 0;
+        x =(b2 - b1)/(k1 - k2);
+        y = k1 * x + b1;
+        System.Console.WriteLine($"Intersectoin point A({x};{y})");
+    }
+
+    else if (k1 == k2 && b1 == b2)
+    {
+        System.Console.WriteLine("Lines coincide");
+    }
+    
+    else if (k1 == k2)
+    {
+        System.Console.WriteLine("Parallel lines, no intersectoin point");
+    }
+}
