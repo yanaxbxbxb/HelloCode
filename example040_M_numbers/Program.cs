a759002aefa022b8daa4dabf17bad111cab3641a
shakirova_yana <ayna.shakirova68@gmail.com>
2022-12-25 16:06:08 +0300

    add example 040 M numbers

---
+++ example040_M_numbers/Program.cs
@@ -0,0 +1,23 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+int M = ReadInt("M");
+
+CountPositiveNumbers(M);
+
+
+int ReadInt(string argument)
+{
+	Console.Write($"Input {argument}: ");
+	return int.Parse(Console.ReadLine()!);
+}
+
+void CountPositiveNumbers(int M)
+{
+    int count = 0;
+    for (int i = 0; i < M; i++)
+    {
+        int number = ReadInt("number");
+        if (number > 0) {count++;}
+    }
+    System.Console.WriteLine($"You input {count} positive numbers");
+}
