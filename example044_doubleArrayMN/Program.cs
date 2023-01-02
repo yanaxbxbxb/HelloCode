35aec29c90a191b7f2a649bf7c21d2ce2c783e6b
shakirova_yana <ayna.shakirova68@gmail.com>
2023-01-02 17:05:42 +0300

    add example 044

---
+++ example044_doubleArrayMN/Program.cs
@@ -0,0 +1,47 @@
+﻿// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+void DoubleArraySizeMN()
+{
+    int M = ReadInt("M");
+    int N = ReadInt("N");
+
+    double[,] array = GetTwoDimensionArray(M, N, -10, 10);
+
+    PrintArray(array);
+}
+
+DoubleArraySizeMN();
+
+int ReadInt(string argument)
+{
+	Console.Write($"Input {argument}:");
+	return int.Parse(Console.ReadLine()!);
+}
+
+double[,] GetTwoDimensionArray(int length1, int length2, int minValue, int maxValue)
+{
+	double[,] array = new double[length1, length2];
+	Random random = new Random();
+
+	for (int i = 0; i < length1; i++)
+	{
+		for (int j = 0; j < length2; j++)
+		{
+			array[i, j] = Math.Round(random.NextDouble() * (maxValue - minValue) + minValue, 2);
+		}
+	}
+	return array;
+}
+
+void PrintArray(double[,] array)
+{
+	for (int i = 0; i < array.GetLength(0); i++)
+	{
+		for (int j = 0; j < array.GetLength(1); j++)
+		{
+			Console.Write($"{array[i, j]} ");
+		}
+		Console.WriteLine();
+	}
+	Console.WriteLine();
+}
